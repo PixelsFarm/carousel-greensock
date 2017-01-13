f122c7bbb47361bba00d7ffeb6e233d648c5a801
hpascual <hpascual@kingeclient.com>
2017-01-13 14:50:40 +0100

    add

--- js/main.js
+++ js/main.js
@@ -1,22 +1,67 @@
 $(document).ready(function(){
 
-    var $slides	= $('.slide');
-    var currentSlide = 0;
-	var slideWidth = $slides.width();
-	var slideLength = $slides.length;
-	console.log(slideWidth);
+//var
+var $slides	= $('.slide'),
+	currentSlide = 0,
+	next = false,
+	slideWidth = $slides.width(),
+	slideLength = $slides.length,
+	restOfSlides = $slides.filter(':gt(0)');
 
-	TweenLite.set($slides.filter(":gt(0)"), {left:slideWidth});
+//ini
+function ini() {
+	TweenLite.set(restOfSlides, {left:slideWidth});
+	//delayNext();
+	bts();
+}
+
+//slides animation
+function delayNext() {
 	TweenLite.delayedCall(4, nextSlide);
+}
 
-	function nextSlide() {
+function nextSlide() {
+	next = true;
+	moveSlide();
+
+	if (currentSlide < slideLength - 1) currentSlide++;
+	else currentSlide = 0;
+
+	TweenLite.fromTo( $slides.eq(currentSlide), 1, {left: slideWidth}, {left:0} );
+	//delayNext();
+}
+
+function prevSlide() {
+	next = false;
+	moveSlide();
+
+	if (currentSlide < slideLength - 1) currentSlide--;
+	else currentSlide = 0;
+
+	TweenLite.fromTo( $slides.eq(currentSlide), 1, {left: -slideWidth}, {left:0} );
+}
+
+function moveSlide() {
+	if (next) {
 		TweenLite.to( $slides.eq(currentSlide), 1, {left:-slideWidth} );
-
-		if (currentSlide < slideLength - 1) currentSlide++;
-		else currentSlide = 0;
-
-		TweenLite.fromTo( $slides.eq(currentSlide), 1, {left: slideWidth}, {left:0} );
-		TweenLite.delayedCall(4, nextSlide);
 	}
-	
+	else {
+		TweenLite.to( $slides.eq(currentSlide), 1, {left: slideWidth} );
+	}
+
+}
+
+//bts
+function bts() {
+	$('.left').click(function(){
+		nextSlide();
+	});
+
+	$('.right').click(function(){
+		prevSlide();
+	});
+}
+
+ini();
+
 });
